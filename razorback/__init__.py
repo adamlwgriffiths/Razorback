01519951628062d06128c9437b0e4052a8c5f46f
Adam Griffiths <adam.lw.griffiths@gmail.com>
2012-10-02 17:51:11 +1000

    Update init to match pygly's latest.
    This doesn't import, instead it constructs
    the __all__ variable.

--- razorback/__init__.py
+++ razorback/__init__.py
@@ -1,9 +1,15 @@
 import pkgutil
 import os
 
+# the version of software
+# this is used by the setup.py script
+from version import __version__
+
 # make 'from module import *' work dynamically.
 # otherwise we have to manually update the __all__ list.
 # http://stackoverflow.com/questions/1057431/loading-all-modules-in-a-folder-in-python
+__all__ = []
+
 for module in os.listdir( os.path.dirname( __file__ ) ):
     name, extension = os.path.splitext( module )
 
@@ -20,7 +26,5 @@ for module in os.listdir( os.path.dirname( __file__ ) ):
     if extension not in extensions: 
         continue
 
-    # file type is ok, import it
-    __import__( name, locals(), globals() )
-    del module
+    __all__.append( name )
 
